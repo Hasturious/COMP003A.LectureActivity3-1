f5d4e2ba8717c9676c3bb9b90e712cf2ba5d38d9
Jean Bryant Figueroa <jbster559@gmail.com>
2025-01-24 14:39:37 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace COMP003A.LectureActivity3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the time?: ");
+            int hour = int.Parse(Console.ReadLine());
+
+            //conditions
+            //5-11
+            //12-17
+            //18-20
+            //21-4
+            if (hour >= 5 && hour <= 11)
+            {
+                Console.WriteLine("Good Morning!");
+            }
+        }
+    }
+}
