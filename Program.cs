804d6c95711812937503be6e9c2871512a0a4dc7
Jean Bryant Figueroa <jbster559@gmail.com>
2025-01-24 14:53:40 -0800

    Added weekday switch case

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("What is the time?: ");
+            Console.Write("Enter the current hour (0-23): ");
             int hour = int.Parse(Console.ReadLine());
 
             //conditions
@@ -12,6 +12,9 @@
             //12-17
             //18-20
             //21-4
+
+            //if and elif statments
+
             if (hour >= 5 && hour <= 11)
             {
                 Console.WriteLine("Good Morning!");
@@ -20,14 +23,41 @@
             {
                 Console.WriteLine("Good Afternoon!");
             }
-            else if (hour >= 12 && hour <= 17)
+            else if (hour >= 18 && hour <= 20)
             {
                 Console.WriteLine("Good Evening!");
             }
-            else if (hour >= 12 && hour <= 17)
+            else if (hour >= 21 && hour <= 4)
             {
                 Console.WriteLine("Good Night!");
             }
+
+            Console.Write("Enter the day of the week: ");
+            string day = Console.ReadLine();
+
+            //condition 1: Monday
+            //condition 2: Friday
+            //condition 3: Sunday
+            //default: Just Another Day
+
+            //switch case
+            
+            switch (day.ToLower())
+            {
+                case "monday":
+                    Console.WriteLine("Start of the work week.");
+                    break;
+                case "friday":
+                    Console.WriteLine("Almost the weekend!");
+                    break;
+                case "sunday":
+                    Console.WriteLine("Relax, it's Sunday.");
+                    break;
+                default:
+                    Console.WriteLine("Just any other day.");
+                    break;
+            }
+
         }
     }
 }
